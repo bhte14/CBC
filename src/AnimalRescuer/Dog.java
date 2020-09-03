76d3d4833e6624f1b4a0dd2faf639fdc9b74b400
bhte14 <cristian.campean12@yahoo.com>
2020-09-03 20:57:05 +0300

    Initial commit

---
+++ src/AnimalRescuer/Dog.java
@@ -0,0 +1,15 @@
+package AnimalRescuer;
+
+public class Dog {
+
+    String name = "Rex";
+    String color = "brown";
+    float age = 0.55f;
+    float weight = 12f;
+    String breed = "Chiuwawawa";
+    boolean isMale = true;
+
+    public void run(){
+
+    }
+}
---
+++ src/AnimalRescuer/Main.java
@@ -0,0 +1,10 @@
+package AnimalRescuer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        dog rex = new Dog();
+        System.out.println(rex);
+    }
+}
